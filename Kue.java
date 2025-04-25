9165fb9e8988bd87c53469e5bb3f534ae2f49f8b
Cheren Agata <cherenads@student.ub.ac.id>
2025-04-25 08:14:21 +0700

    Create Kue.java

---
+++ Kue.java
@@ -0,0 +1,18 @@
+package yum;
+
+public abstract class Kue {
+    protected String nama;
+    protected double harga;
+
+    public Kue(String nama, double harga) {
+        this.nama = nama;
+        this.harga = harga;
+    }
+
+    public abstract double hitungHarga();
+
+    @Override
+    public String toString() {
+        return "Nama: " + nama + ", Harga Satuan: " + harga;
+    }
+}
