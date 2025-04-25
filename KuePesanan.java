a64f839f619888d48bc5d62fe977e0ca9919f2e1
Cheren Agata <cherenads@student.ub.ac.id>
2025-04-25 08:15:26 +0700

    Create KuePesanan.java

---
+++ KuePesanan.java
@@ -0,0 +1,25 @@
+package yum;
+
+public class KuePesanan extends Kue {
+    private double berat;
+
+    public KuePesanan(String nama, double harga, double berat) {
+        super(nama, harga);
+        this.berat = berat;
+    }
+
+    @Override
+    public double hitungHarga() {
+        return harga * berat;
+    }
+
+    public double getBerat() {
+        return berat;
+    }
+
+    @Override
+    public String toString() {
+        return "[KuePesanan] " + super.toString() + ", Berat: " + berat + ", Total Harga: " + hitungHarga();
+    }
+}
+
